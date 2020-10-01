28c13b8bb8818d33926bca0a1bae193732fd03c2
Alice-Audureau <alice.audureau@gmail.com>
2020-10-01 16:45:17 +0200

    ajout wilders

---
+++ Node.js
@@ -0,0 +1,11 @@
+console.log("Alice")
+console.log("Maxime")
+console.log("Fanny")
+console.log("Nicolas")
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Alice'));
+console.log(chalk.red('Maxime'));
+console.log(chalk.yellow('Fanny'));
+console.log(chalk.green('Nicolas'));
